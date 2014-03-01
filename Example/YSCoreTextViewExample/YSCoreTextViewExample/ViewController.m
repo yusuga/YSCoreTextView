794598292a6214f8f00febc2d87fdba126032576
Yu Sugawara <yusuga@me.com>
2014-03-01 15:07:39 +0900

    Add Example

---
+++ Example/YSCoreTextViewExample/YSCoreTextViewExample/ViewController.m
@@ -0,0 +1,59 @@
+//
+//  ViewController.m
+//  YSCoreTextViewExample
+//
+//  Created by Yu Sugawara on 2014/03/01.
+//  Copyright (c) 2014年 Yu Sugawara. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "YSCoreTextView.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet YSCoreTextView *textView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    UIFont *font = [UIFont systemFontOfSize:25.];
+    CFStringRef fontName = (__bridge CFStringRef)font.fontName;
+    CGFloat fontSize = font.pointSize;
+	CTFontRef ctfont = CTFontCreateWithName(fontName, fontSize, NULL);
+    
+    NSString *text = @"Simple drawing of the CoreText.";
+    
+    YSCoreTextLayout *layout = [[YSCoreTextLayout alloc] initWithConstraintWidth:self.textView.bounds.size.width
+                                                                            text:text
+                                                                       attributes:@{(id)kCTFontAttributeName : (__bridge id)ctfont,
+                                                                                    (id)kCTForegroundColorAttributeName : (__bridge id)[UIColor blueColor].CGColor}];
+    CFRelease(ctfont);
+    
+    layout.hightlight = @[[YSCoreTextHighlight highlightWithRange:NSMakeRange(1, 3) color:[[UIColor blueColor] colorWithAlphaComponent:0.4]],
+                          [YSCoreTextHighlight highlightWithRange:NSMakeRange(19, 5) color:[[UIColor yellowColor] colorWithAlphaComponent:0.4]]];
+    
+    CGRect frame = self.textView.frame;
+    frame.size = layout.size;
+    self.textView.frame = frame;
+    NSLog(@"%@", NSStringFromCGRect(self.textView.frame));
+    
+    [self.textView drawWithLayout:layout];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
